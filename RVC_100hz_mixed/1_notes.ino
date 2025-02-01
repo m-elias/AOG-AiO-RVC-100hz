cc4e1e4755443d849be07645031851f74006f3a6
m-elias <eliaschief@gmail.com>
2025-01-31 18:34:26 -0600

    updated notes

--- RVC_100hz_mixed/1_notes.ino
+++ RVC_100hz_mixed/1_notes.ino
@@ -29,26 +29,28 @@ Dual Operation Logic
 
 Autosteer updates at 100hz but should maybe only be at the old 40hz?
 
-
 Machine/Section outputs
 - only supported by AiO v5.0 Proto
   - v5.0a-c uses Wire1 (I2C1) to connect to PCA9555
     - v4.x uses Wire1 to connect to ADS1115
-  - v5.0d uses Wire to connect to PCA9685 for Machine/Sections and RGB LEDs
+  - v5.0d uses Wire to connect to (2) PCA9685, for Machine/Sections and RGB LEDs
 
+Sends all UDP PGNs to ESP32 via UART
+- relays PGNs from ESP32 UART back to AgIO via UDP
 
 
 To-do
 - Adafruit NeoPixel (WS2811) library disabled interrupts which causes lost Serial data
-  - this has been overriden to keep interrupts on, LEDs don't blink quite 100%
-  - for v5.0d, add code for WS2812Serial library for nonblock RGB control
-- send more data to ESP32, such as speed and roll correction position
+  - this has been overridden to keep interrupts on, LEDs don't blink quite 100%
+    - to fix add code for WS2812Serial library for nonblock RGB control and move output pin to a UART TX pin
+  - v5.0d switched to PCA9685 PWM I2C extender instead of WS2811
 - set ADS1115 single/dual according to config struct setting (saved in eeprom)
 - consolidate all EEPROM addrs in one place?
     - Ethernet, Autosteer, machine
 - test/fix autosteer watch dog timeout from lost comms
 - write piezo class
 - expand machine/PCA9555 to monitor outputs with PCA's extra input pins
+  - no longer applicable for new versions
 - add analog PCB ID input
 - use 2nd Eth jack LED for something?
 - intelligently detect Eth link status?
